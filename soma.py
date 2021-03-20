cb95a7395d8a74b49e36d0c5e94cace454343986
Clenia Guedes <cleniaguedes1@gmail.com>
2021-03-20 18:12:21 -0300

    added choose the calc between sum and subtraction

--- soma.py
+++ soma.py
@@ -1,4 +1,8 @@
 first_number = int(input("type the first value \n"))
 second_number = int(input("type the second value \n"))
-sum = first_number + second_number
-print(f"the value is: {sum}")
+option = int(input(" 1- to sum \n 2 - to subtract \n"))
+if(option == 1):
+    calc = first_number + second_number
+else:
+    calc = first_number - second_number
+print(f"the value is: {calc}")
